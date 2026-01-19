7fc0a737f25c13715b10d94158bda5e4982f597a
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2026-01-19 11:14:14 +0530

    Fix indentation for prime number check

--- mathOpe.py
+++ mathOpe.py
@@ -12,8 +12,8 @@ else:
     print("It is an odd number")
 
 
-    for i in range(2, num):
-        if num % i == 0:
+for i in range(2, num):
+    if num % i == 0:
             print("It is not a prime number")
             break
     else:
