8c839f931c03dbaf1958c171ce8cd54ce19cd3dd
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2026-01-19 11:00:48 +0530

    Save

---
+++ mathOpe.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv==2):
+    num=int(sys.argv[1])
+else:
+    num=3
+
+if num%2==0:
+    print("It is a even number")
+else:
+    print("It is a odd number")
+
+for i in range(1,11):
+    if num%i==0:
+        print("it is not a prime number")
+    else:
+        print("It is a prime number")
