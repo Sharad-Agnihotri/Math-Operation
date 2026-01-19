feb2143291be41b8672954c99413aa7843e7ec0a
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2026-01-19 11:12:09 +0530

    Refactor number checks for clarity and consistency

--- mathOpe.py
+++ mathOpe.py
@@ -1,17 +1,23 @@
 import sys
 
-if len(sys.argv)==2:
-    num=int(sys.argv[1])
+if len(sys.argv) == 2:
+    num = int(sys.argv[1])
 else:
-    num=3
+    num = 3
 
-if num%2==0:
-    print("It is a even number")
+
+if num % 2 == 0:
+    print("It is an even number")
 else:
-    print("It is a odd number")
+    print("It is an odd number")
 
-for i in range(2,num):
-    if num%i==0:
-        print("it is not a prime number")
+
+if num <= 1:
+    print("It is not a prime number")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("It is not a prime number")
+            break
     else:
         print("It is a prime number")
