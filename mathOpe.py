18220567ce693b7455fee004e1a1012a4b1f9723
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2026-01-19 11:22:37 +0530

    Refactor prime number check logic in mathOpe.py

--- mathOpe.py
+++ mathOpe.py
@@ -5,17 +5,18 @@ if len(sys.argv) == 2:
 else:
     num = 3
 
-
 if num % 2 == 0:
     print("It is an even number")
 else:
     print("It is an odd number")
 
 
-for i in range(1, num):
-    if num % i == 0:
+if num <= 1:
+    print("It is not a prime number")
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             print("It is not a prime number")
             break
     else:
         print("It is a prime number")
-
